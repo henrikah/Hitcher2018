3bfedc2f84d01db29e15f6ba5c7c0c4a347ec55e
Henrik Helgesen <henrik.helgesen@gmail.com>
2017-09-29 13:34:17 +0200

    Revert "Revert "Add project files.""
    This reverts commit 9b403a8cf15bdb4c3e60dfa54bb20f46787ec43c.

---
+++ HitcherAPI/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace HitcherAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
---
+++ HitcherAPI/Areas/HelpPage/ModelDescriptions/IModelDocumentationProvider.cs
@@ -0,0 +1,12 @@
+using System;
+using System.Reflection;
+
+namespace HitcherAPI.Areas.HelpPage.ModelDescriptions
+{
+    public interface IModelDocumentationProvider
+    {
+        string GetDocumentation(MemberInfo member);
+
+        string GetDocumentation(Type type);
+    }
+}
